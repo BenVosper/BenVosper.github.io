bd1030c26ea48895da1af80d2d3b17df419d6c1f
BenVosper <ben-vosper@hotmail.com>
2020-01-31 20:37:46 +0000

    Use prettier instead of eslint

--- src/init.js
+++ src/init.js
@@ -1,31 +1,34 @@
-import { getBackground, elementToString } from './svg.js';
+import { getBackground, elementToString } from "./svg.js";
 
-const svgDataURLPrefix = 'data:image/svg+xml;utf8,';
+const svgDataURLPrefix = "data:image/svg+xml;utf8,";
 
-const backgroundElementID = 'background';
+const backgroundElementID = "background";
 const zigCenter = [50, 50];
 const zigInnerRadius = 10;
 const zigOuterRadius = 40;
 const nPoints = 300;
 
-
 function setBackgroundImage() {
   const background = document.getElementById(backgroundElementID);
-  const backgroundSVG = getBackground(zigCenter, zigInnerRadius, zigOuterRadius, nPoints);
+  const backgroundSVG = getBackground(
+    zigCenter,
+    zigInnerRadius,
+    zigOuterRadius,
+    nPoints
+  );
   const backgroundSVGString = elementToString(backgroundSVG);
   const backgroundImageString = `url('${svgDataURLPrefix}${backgroundSVGString}')`;
   background.style.backgroundImage = backgroundImageString;
 }
 
-const linksList = document.getElementById('links-list')
+const linksList = document.getElementById("links-list");
 
 function toggleLinksList() {
   const display = linksList.style.display;
-  linksList.style.display = display === 'none' ? '' : 'none'
+  linksList.style.display = display === "none" ? "" : "none";
 }
 
-
 setBackgroundImage();
-const links = document.getElementById('links')
-links.addEventListener('click', toggleLinksList)
-toggleLinksList()
+const links = document.getElementById("links");
+links.addEventListener("click", toggleLinksList);
+toggleLinksList();
